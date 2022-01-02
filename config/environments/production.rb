6f9cd2586245052d1cb69db151ce7963146ed58b
Tran <vansang10a6txqt@gmail.com>
2022-01-02 16:25:36 +0700

    á

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,6 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  config.action_mailer.default_url_options = { :host => "thawing-refuge-11069.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
