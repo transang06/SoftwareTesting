caedee758cb51c8eea69c57dc3be492c5157bfbc
Tran <vansang10a6txqt@gmail.com>
2022-01-02 16:30:33 +0700

    Á

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,9 +61,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "dn_intern_mangement_hotel_06_09_21_production"
-
-  config.action_mailer.perform_caching = false
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     address: "smtp.mail.com",
     port: 587,
     domain: "thawing-refuge-11069.herokuapp.com",
@@ -72,11 +70,13 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
+  ActionMailer::Base.delivery_method = :smtp
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "thawing-refuge-11069.herokuapp.com" }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
