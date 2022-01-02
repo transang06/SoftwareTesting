c3ea3c368c4a95a9cfe18f87b2b281ec3ff963cd
Tran <vansang10a6txqt@gmail.com>
2022-01-02 15:58:07 +0700

    á

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,20 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "dn_intern_mangement_hotel_06_09_21_production"
 
   config.action_mailer.perform_caching = false
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+
+  ActionMailer::Base.delivery_method = :smtp
+
   config.action_mailer.default_url_options = { :host => "thawing-refuge-11069.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
