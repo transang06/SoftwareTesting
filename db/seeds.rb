16e4e375e15f2a2d3fb546788d6edbaf730a70d3
Tran <vansang10a6txqt@gmail.com>
2022-01-02 21:47:00 +0700

    á

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
-# User.create!(first_name: "Trần",
-#   last_name: "Văn Sáng",
-#   email: "vansang10a6txqt@gmail.com",
-#   password: "vansang12",
-#   password_confirmation: "vansang12",
-#   phone: "0349934988",
-#   role: 1,
-#   confirmed_at: Time.zone.now)
+User.create!(first_name: "Trần",
+  last_name: "Văn Sáng",
+  email: "vansang10a6txqt@gmail.com",
+  password: "vansang12",
+  password_confirmation: "vansang12",
+  phone: "0349934988",
+  role: 1,
+  confirmed_at: Time.zone.now)
 
-# User.create!(first_name: "Trần",
-#   last_name: "Văn Sáng",
-#   email: "vansang@gmail.com",
-#   password: "vansang12",
-#   password_confirmation: "vansang12",
-#   phone: "0349934988",
-#   confirmed_at: Time.zone.now)
+User.create!(first_name: "Trần",
+  last_name: "Văn Sáng",
+  email: "vansang@gmail.com",
+  password: "vansang12",
+  password_confirmation: "vansang12",
+  phone: "0349934988",
+  confirmed_at: Time.zone.now)
 
 30.times do |n|
   name = Faker::House.room
@@ -24,8 +24,9 @@
     hourly_price: price,
     day_price: price*8,
     monthly_price: price*89,
-    description: Faker::Restaurant.description,
-    photo: File.open(Rails.root.join("app/assets/images/room.jpg")))
+    description: Faker::Restaurant.description)
+    room.images.attach(io: File.open(Rails.root.join("app/assets/images/room.jpg")),filename: "room.jpg")
+    room.images.attach(io: File.open(Rails.root.join("app/assets/images/room2.jpg")),filename: "room2.jpg")
 end
 
 rooms = Room.order(:created_at).take(10)
